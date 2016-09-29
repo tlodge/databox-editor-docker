ab97af56564a095605e7ff15549998594ac41bb2
tlodge <tlodge@gmail.com>
2016-09-29 16:32:45 +0100

    cp commit

--- node-red-databox/build.sh
+++ node-red-databox/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -t databox/red .
+docker build --no-cache -t databox/red .
 docker tag databox/red registry.upintheclouds.org/databox/red && docker push registry.upintheclouds.org/databox/red
