f09a0d8cc661d0ecc4c6b49916941cecd67d1cbe
tlodge <tlodge@gmail.com>
2016-10-24 10:56:32 +0100

    scripts update

--- scripts/updatenodes.sh
+++ scripts/updatenodes.sh
@@ -4,4 +4,5 @@ cd $ROOT/databox-nodered-nodes && git add . && git commit -m 'cp commit' && git 
 docker rm $(docker stop $(docker ps -a -q --filter ancestor="docker_red" --format="{{.ID}}"))
 docker rm $(docker stop $(docker ps -a -q --filter ancestor="databox/testred" --format="{{.ID}}"))
 cd $ROOT/docker/node-red-tester/ && ./build.sh
+cd $ROOT/docker/node-red-databox/ && ./build.sh
 cd $ROOT/docker && docker-compose up -d
