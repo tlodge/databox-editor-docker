9e67fbbc0c702bc90b13834dc04e6166f19b8130
tlodge <tlodge@gmail.com>
2016-12-02 13:45:45 +0000

    separated out build and deploy

--- node-red-databox/build.sh
+++ node-red-databox/build.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
 docker build --no-cache -t databox/red .
-docker tag databox/red registry.upintheclouds.org/databox/red && docker push registry.upintheclouds.org/databox/red
-docker tag databox/red registry.iotdatabox.com/databox/red && docker push registry.iotdatabox.com/databox/red
--- node-red-tester/build.sh
+++ node-red-tester/build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 docker build --no-cache -t databox/testred .
-docker tag databox/testred registry.iotdatabox.com/databox/testred && docker push registry.iotdatabox.com/databox/testred
