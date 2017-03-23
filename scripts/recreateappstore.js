37a41b20d24e54819e9e249871e4feebe3bba8cf
tlodge <tlodge@gmail.com>
2017-03-23 13:01:58 +0000

    updated the update store script

--- scripts/recreateappstore.js
+++ scripts/recreateappstore.js
@@ -2,9 +2,9 @@ var fs = require('fs');
 var path =require('path');
 var request= require('superagent');
 
-var _saveToAppStore = function(manifest){
+var _saveToAppStore = function(store, manifest){
 	request
-  		.post("http://store.upintheclouds.org/app/post")
+  		.post(store + "/app/post")
   		.send(manifest)
  		.set('Accept', 'application/json')
   		.type('form')
@@ -17,14 +17,16 @@ var _saveToAppStore = function(manifest){
   	 	})	
 };
 
-if (process.argv.length < 3) {
-        console.log("node recreateappstore  <manifestdirectory>");
+if (process.argv.length < 4) {
+        console.log("node recreateappstore  <manifestdirectory> <store eg: http://store.upintheclouds.org>");
         process.exit(1);
 }
 
 var dir = process.argv[2];
-
 console.log("reading dir " + dir);
+var store = process.argv[3];
+console.log("writing to store");
+console.log(store);
 
 fs.readdir(dir, function(err, items) {
 	items.filter(function(item){
@@ -46,7 +48,7 @@ fs.readdir(dir, function(err, items) {
 										
 			queries: JSON.stringify(0),
 		}; 
-      		_saveToAppStore(data);
+      		_saveToAppStore(store,data);
     	    });	
 	});
 });
