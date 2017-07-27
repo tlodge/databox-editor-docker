2e310f44fc15d58ac8857f52157b366813676724
tlodge <tlodge@gmail.com>
2017-07-27 20:53:05 +0100

    cp commit

--- node-red-tester/deploy.sh
+++ node-red-tester/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker tag databox/testred registry.iotdatabox.com/databox/testred && docker push registry.iotdatabox.com/databox/testred
+docker tag databox/testred tlodge/databox-red && docker push tlodge/databox-red
